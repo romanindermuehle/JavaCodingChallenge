134082a7490c797c3be56db2abda0c67af6f269c
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-09 07:46:58 +0200

    MergeSort with tests added

--- src/main/java/CustomSort.java
+++ src/main/java/CustomSort.java
@@ -17,11 +17,55 @@ public class CustomSort {
         return result;
     }
 
-    public static int[] quickSort(int[] numbers) {
-        int[] result = Arrays.copyOf(numbers, numbers.length);
+    public static int[] mergeSort(int[] numbers) {
+        if (numbers.length <= 3) {
+            return bubbleSort(numbers);
+        }
 
+        int middle = numbers.length >>> 1;
 
+        int[] part1 = Arrays.copyOfRange(numbers, 0, middle);
+        int[] part2 = Arrays.copyOfRange(numbers, middle, numbers.length);
+
+        int[] sortedPart1 = mergeSort(part1);
+        int[] sortedPart2 = mergeSort(part2);
+
+        int[] result = merge(sortedPart1, sortedPart2);
 
         return result;
     }
+
+    public static int[] merge(int[] a, int[] b) {
+        int resultLenght = a.length + b.length;
+        int[] result = new int[resultLenght];
+
+        for (int resultIndex = 0, part1Index = 0, part2Index = 0; resultIndex < resultLenght; resultIndex++) {
+
+//            if (part1Index > a.length) {
+//                result.add(b[part2Index]);
+//                part2Index++;
+//            } else if (part2Index > b.length) {
+//                result.add(a[part1Index]);
+//                part1Index++;
+//            } else if (a[part1Index] < b[part2Index]) {
+//                result.add(a[part1Index]);
+//                part1Index++;
+//            } else {
+//                result.add(b[part2Index]);
+//                part2Index++;
+//            }
+
+            if (part2Index >= b.length || (part1Index < a.length && a[part1Index] < b[part2Index])) {
+                result[resultIndex] = a[part1Index];
+                part1Index++;
+            } else {
+                result[resultIndex] = b[part2Index];
+                part2Index++;
+            }
+        }
+        return result;
+    }
+
+
+    //TODO quickSort
 }
--- src/test/java/CustomSortTest.java
+++ src/test/java/CustomSortTest.java
@@ -1,11 +1,45 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CustomSortTest {
     @Test
     public void bubbleSort() {
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, CustomSort.bubbleSort(new int[]{9, 8, 2, 3, 5, 7, 1, 4, 6, 10}));
         assertArrayEquals(new int[]{1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10}, CustomSort.bubbleSort(new int[]{9, 8, 2, 3, 5, 7, 1, 4, 6, 10, 5}));
+    }
+
+    @Test
+    public void mergeSort() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, CustomSort.mergeSort(new int[]{9, 8, 2, 3, 5, 7, 1, 4, 6, 10}));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10}, CustomSort.mergeSort(new int[]{9, 8, 2, 3, 5, 7, 1, 4, 6, 10, 5}));
+    }
+
+    @Test
+    public void merge() {
+        assertArrayEquals(new int[]{2, 1, 3, 9, 10, 9}, CustomSort.merge(new int[]{2, 1, 3}, new int[]{9, 10, 9}));
+    }
+
+    @Test
+    public void compareSortPerformance() {
+        int[] values = new Random().ints(10000, 0,100).toArray();
+
+        long t1 = System.nanoTime();
+        CustomSort.bubbleSort(values);
+        long t2 = System.nanoTime();
+
+        long t3 = System.nanoTime();
+        CustomSort.mergeSort(values);
+        long t4 = System.nanoTime();
+
+        System.out.println("Bubble: " + TimeUnit.NANOSECONDS.toMillis(t2 - t1) + "ms");
+
+        System.out.println("Merge: " + TimeUnit.NANOSECONDS.toMillis(t4 - t3) + "ms");
     }
 }
