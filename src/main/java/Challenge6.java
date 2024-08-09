ce88b15aee7fd929d3cadd683bfcf3f6e20ddfc2
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-09 11:03:37 +0200

    RemoveDuplicateLetters logic and tests added

--- src/main/java/Challenge6.java
+++ src/main/java/Challenge6.java
@@ -1,2 +1,13 @@
+import java.util.LinkedHashSet;
+
 public class Challenge6 {
+    public static String removeDuplicateLetters(String input) {
+        LinkedHashSet<String> withoutDuplicate = new LinkedHashSet<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            withoutDuplicate.add(String.valueOf(input.charAt(i)));
+        }
+
+        return String.join("", withoutDuplicate);
+    }
 }
--- src/test/java/Challenge6Test.java
+++ src/test/java/Challenge6Test.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class Challenge6Test {
+    @Test
+    public void removeDuplicates() {
+        assertEquals("helo", Challenge6.removeDuplicateLetters("hello"));
+        assertEquals("wombat", Challenge6.removeDuplicateLetters("wombat"));
+        assertEquals("Misp", Challenge6.removeDuplicateLetters("Mississippi"));
+    }
 }
